727d238f0b9124012d4c1c5cd5430a33d7ece7b4
mo <1maalinma@gmail.com>
2025-01-12 14:13:38 -0500

    create a new deck

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,19 +1,35 @@
 <template>
 	<div>
 		<ul>
+			<div>
+				<p v-if="complete">
+					made a new deck {{this.new_deck_name}}
+				</p>
+			</div>
+
 			<li><router-link to="/deck">deck</router-link></li>
+
 			<div v-if="error">
 				error!
 			</div>
+
+
 			<div>
 				you are viewing {{selected}}
 				<select v-model="selected">
 					<option disabled value="">please select a deck</option>
 					<option v-for="deck in this.decks" :key="deck.id">
-						<option>{{deck.name}}</option>
+						{{deck.name}}
 					</option>
 				</select>
 			</div>
+
+			<div>
+				<p>make a new deck called {{new_deck_name}}</p>
+				<input v-model="new_deck_name">
+				<button @click="new_deck(new_deck_name)">submit</button>
+			</div>
+
 		</ul>
 	</div>
 </template>
@@ -25,7 +41,10 @@
 			return {
 				error: false,
 				decks: [],
-				selected: 'no deck'
+				selected: 'no deck',
+				new_deck_name: '',
+				complete: false
+
 			}
 		},
 
@@ -36,7 +55,26 @@
 					this.decks = res.data.decks
 				} catch (e) {
 					this.error = true
+				} finally {
+					this.error = false
 				}
+
+			},
+
+			async new_deck(new_deck_name) {
+				try {
+					await axios.post('http://localhost:5000/deck', {
+						name: new_deck_name
+					})
+
+				} catch (e) {
+					this.error = true
+				} finally {
+					this.complete = true
+					this.get_decks()
+					setTimeout(() => {this.complete = false}, 10000);
+				}
+				
 			}
 		},
 
